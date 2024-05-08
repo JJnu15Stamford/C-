ceeb66eb6f269205ddeaf919753e650536f1d2f4
JJnu15Stamford <jocuch13@icloud.com>
2024-05-08 17:57:01 -0400

    initial commit

---
+++ c#_1/char helpfulApp = 90%.cs
@@ -0,0 +1,19 @@
+static void Main (string[args])
+char scaryGame = 90%
+Console.WriteLine("Hi!");
+
+
+int n = 100; 
+int add()
+
+public interface playerHealth = 100;
+private string secret = ("the house of info");
+int numbers 
+// accessing each individual element 
+int[] myArray = new int []
+myArray[0] = 
+myArray[1] = 
+myArray[2] = 
+myArray[3] = 
+myArray[4] =
+// using the collection 
